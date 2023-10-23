4b21cd0f1e2420f3b2d19c3bdebf24c7f53ee9b2
renderTom <tomas@rendertom.com>
2023-10-23 18:39:49 +0300

    add mona

---
+++ praktika/main-mona.py
@@ -0,0 +1,21 @@
+from lib.Audio import Audio
+from lib.Bitmap import Bitmap
+from lib.Piano import Piano
+from lib.Tempo import Tempo
+
+image_path = "images/mona.png"
+image = Bitmap(path=image_path)
+print(image.toString())
+
+piano = Piano(note_start='A3', note_end='E6')
+audio = Audio(image, piano,
+          pixel_duration = Tempo("presto").get_note_duration(1/16),
+          num_channels = 2
+        )
+
+audio.process()
+print(audio.toString())
+
+audio.save()
+audio.play()
+audio.plot()
